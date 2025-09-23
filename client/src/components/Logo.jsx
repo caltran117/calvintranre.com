ac8c8b97f7d47f7d5d3470f03076550b90109a4a
caltran117 <caltran117@gmail.com>
2025-09-22 22:36:57 -0400

    logo

--- client/src/components/Logo.jsx
+++ client/src/components/Logo.jsx
@@ -11,25 +11,11 @@ const Logo = () => {
         whileHover={{ scale: 1.05 }}
         whileTap={{ scale: 0.95 }}
       >
-        <div className="h-12 w-12 bg-red-600 flex items-center justify-center mr-2">
-          <svg 
-            className="h-6 w-6 text-white" 
-            xmlns="http://www.w3.org/2000/svg" 
-            viewBox="0 0 24 24" 
-            fill="none" 
-            stroke="currentColor" 
-            strokeWidth="2" 
-            strokeLinecap="round" 
-            strokeLinejoin="round"
-          >
-            <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
-          </svg>
-        </div>
-        <div className="text-gray-800">
-          <div className="text-sm font-bold -mt-1">CALVIN</div>
-          <div className="text-sm font-bold -mt-1">TRAN</div>
-          <div className="text-sm font-bold -mt-1">RE</div>
-        </div>
+        <img 
+          src="/images/Asset 4@3x.png" 
+          alt="Calvin Tran RE Logo" 
+          className="h-18 w-auto object-contain" 
+        />
       </motion.div>
     </Link>
   );
